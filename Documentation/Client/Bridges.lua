459938f08a6d5bf97540bc9144a271c182b71347
Luis Fernando Más Ortiz <louisteam@hotmail.com>
2025-04-10 09:57:52 -0300

    Doc Client

---
+++ Documentation/Client/Bridges.lua
@@ -0,0 +1,79 @@
+BridgeFunctionAttach('MainLoader','TemplateScript_MainLoader')
+BridgeFunctionAttach('MainProcThread','TemplateScript_MainProcThread')
+BridgeFunctionAttach('MainInterfaceProcThread','TemplateScript_MainInterfaceProcThread')
+BridgeFunctionAttach('OnReadScript','TemplateScript_OnReadScript')
+BridgeFunctionAttach('OnShutScript','TemplateScript_OnShutScript')
+BridgeFunctionAttach('KeyboardEvent','TemplateScript_KeyboardEvent')
+BridgeFunctionAttach('ScrollMouseEvent','TemplateScript_ScrollMouseEvent')
+BridgeFunctionAttach('RightClickEvent','TemplateScript_RightClickEvent')
+BridgeFunctionAttach('LeftClickEvent','TemplateScript_LeftClickEvent')
+BridgeFunctionAttach('UpdateMouseEvent','TemplateScript_UpdateMouseEvent')
+BridgeFunctionAttach('UpdateKeyEvent','TemplateScript_UpdateKeyEvent')
+BridgeFunctionAttach('ClientProtocol','TemplateScript_ClientProtocol')
+
+
+function TemplateScript_MainLoader()
+	-- ### Bridge information: ###
+	-- Called once at Client is loading files.
+end
+
+function TemplateScript_MainProcThread()
+	-- ### Bridge information: ###
+	-- Called in real time client proccess.
+end
+
+function TemplateScript_MainInterfaceProcThread()
+	-- ### Bridge information: ###
+	-- Called in real time interface proccess.
+end
+
+function TemplateScript_OnReadScript()
+	-- ### Bridge information: ###
+	-- Called once at GameServer initialization.
+end
+
+function TemplateScript_OnShutScript()
+	-- ### Bridge information: ###
+	-- Called once before script reload.
+end
+
+function TemplateScript_KeyboardEvent(KeyNumber)
+	-- ### Argument information: ###
+	-- KeyNumber = keyboard key number - Check Defines.lua
+
+	-- ### Bridge information: ###
+	-- Called after keyboard key pressed.
+end
+
+function TemplateScript_ScrollMouseEvent(ScrollData)
+	-- ### Argument information: ###
+	-- ScrollData = Scroll action.
+
+	-- ### Bridge information: ###
+	-- Called after use the mouse scroll.
+end
+
+function TemplateScript_RightClickEvent()
+	-- ### Bridge information: ###
+	-- Called after mouse right click.
+end
+
+function TemplateScript_LeftClickEvent()
+	-- ### Bridge information: ###
+	-- Called after mouse left click.
+end
+
+function TemplateScript_UpdateMouseEvent()
+	-- ### Bridge information: ###
+	-- Called in real time interface update.
+end
+
+function TemplateScript_UpdateKeyEvent()
+	-- ### Bridge information: ###
+	-- Called in real time interface key update.
+end
+
+function TemplateScript_ClientProtocol(head, packet_name)
+	-- ### Bridge information: ###
+	-- Called after receive some LUA packet.
+end
---
+++ Documentation/Client/Functions.lua
@@ -0,0 +1,152 @@
+-- ### BASE FUNCTIONS ###
+Console(Color,Text)
+RenderMessage(Text,Type)
+LoadBitmap(Path,ImagemID)
+LoadSound(SoundID,Folder)
+PlaySound(SoundID,Stop)
+MousePosX() return int
+MousePosY() return int
+MouseLButton() return int
+MouseRButton() return int
+CheckMouseIn(PosX,PosY,Width,Height) return int
+GetMainResolution() return int
+GetMainScene() return int
+GetCharacterMap() return int
+CheckWindowOpen() return int
+CharacterLockAction(Lock)
+SetWindowTitle(Title)
+
+-- ### USER FUNCTIONS ###
+UserGetMap() return integer
+UserGetName() return string
+UserGetIndex() return integer
+UserPositionX() return integer
+UserPositionY() return integer
+UserGetHelper() return integer
+UserGetWing() return integer
+UserGetSword() return integer
+UserGetShield() return integer
+UserUserGetHelm() return integer
+UserGetArmor() return integer
+UserGetPants() return integer
+UserGetGloves() return integer
+UserGetBoots() return integer
+UserGetLevel() return integer
+UserGetClass() return integer
+UserGetGuild() return integer
+GuildGetName() return string
+
+-- ### CHARACTER FUNCTIONS ###
+CharacterGetName(int arrayIndex) return string
+CharacterGetIndex(int arrayIndex) return integer
+CharacterGetIsLive(int arrayIndex) return integer
+CharacterGetType(int arrayIndex) return integer
+CharacterGetX(int arrayIndex) return integer
+CharacterGetY(int arrayIndex) return integer
+CharacterGetHelper(int arrayIndex) return integer
+CharacterGetWing(int arrayIndex) return integer
+CharacterGetSword(int arrayIndex) return integer
+CharacterGetShield(int arrayIndex) return integer
+CharacterGetHelm(int arrayIndex) return integer
+CharacterGetArmor(int arrayIndex) return integer
+CharacterGetPants(int arrayIndex) return integer
+CharacterGetGloves(int arrayIndex) return integer
+CharacterGetBoots(int arrayIndex) return integer
+CharacterGetLevel(int arrayIndex) return integer
+CharacterGetClass(int arrayIndex) return integer
+CharacterGetGuild(int arrayIndex) return integer
+CharacterGuildGetName(int arrayIndex) return string
+SendPlayerChat(string aValue)
+FindCharacterStack(int index) return integer
+GetSelectedHero() return integer
+SetSelectedHero(int value)
+GetTargetCharacter() return integer
+SetTargetCharacter(int value)
+SendTradePlayer(int index)
+SendPartyPlayer(int index)
+SendGuildPlayer(int index)
+SendShopPlayer(int index)
+CharacterGetPositionX(int arrayIndex) return float
+CharacterSetPositionX(int arrayIndex, float value)
+CharacterGetPositionY(int arrayIndex) return float
+CharacterSetPositionY(int arrayIndex, float value)
+CharacterGetPositionZ(int arrayIndex) return float
+CharacterSetPositionZ(int arrayIndex, float value)
+CharacterGetAngleX(int arrayIndex) return float
+CharacterSetAngleX(int arrayIndex, float value)
+CharacterGetAngleY(int arrayIndex) return float
+CharacterSetAngleY(int arrayIndex, float value)
+CharacterGetAngleZ(int arrayIndex) return float
+CharacterSetAngleZ(int arrayIndex, float value) 
+CharacterSetScale(int arrayIndex, float value)
+GetClassName(int Class) return string
+CharacterGetGuildStatus(int arrayIndex) return integer
+CharacterGetVisible(int arrayIndex) return integer
+SetCharacterAction(int arrayIndex, int animation)
+CharacterGetFullClass(int arrayIndex) return integer
+
+-- ### INTERFACE FUNCTIONS ###
+RenderImage(int imageID, float x, float y, float width, float height)
+MousePosX() return integer 
+MousePosY() return integer 
+glColor3f(float r, float g, float b)
+glColor4f(float r, float g, float b, float a)
+DisableAlphaBlend()
+pSetBlend(int aValue) --EnableAlphaTest
+DrawBar(float x, float y, float w, float h)
+EndDrawBar()
+SetFontType(int font)
+SetFontBg(int r, int g, int b, int a)
+SetTextColor(int r, int g, int b, int a)
+RenderText(int x, int y, char* text, int width, int sort)
+RenderImage(int imageIndex, float x, float y, float w, float h)
+RenderBitmap(int Texture, float x, float y, float w, float h, float u, float v, float uWidth, float vHeight, int Scale, int StartScale, float Alpha)
+RenderImageScale(int Texture, float x, float y, float w, float h, float u, float v, float uWidth, float vHeight, float Alpha)
+CheckIsRepeatKey(int Key) return integer 
+CheckPressedKey(int Key) return integer 
+CheckReleasedKey(int Key) return integer 
+CheckWindowOpen(int Key) return integer 
+CloseWindow(int Key)
+OpenWindow(int Key)
+ResetMouseL()
+ResetMouseR()
+DisableCLick()
+CheckClickClient() return integer 
+GetWindowWidth() return integer 
+GetWindowHeight() return integer 
+GetImageWidth(int texture) return float 
+GetImageHeight(int texture) return float 
+ItemByteConvert(BYTE* lpMsg, int ItemIndex, int Level, int dur, int Op1, int Op2, int Op3, int Exc, int Ancient, int JoH, int Socket, BYTE Sock1, BYTE Sock2, BYTE Sock3, BYTE Sock4, BYTE Sock5)
+RenderTooltipComplete(int x, int y, int ItemIndex, int Level, int dur, int Op1, int Op2, int Op3, int Exc, int Ancient, int JoH, int Socket, int Sock1, int Sock2, int Sock3, int Sock4, int Sock5)
+RenderTooltip(int aValue,int bValue,int cValue,int dValue,int eValue,int fValue,int gValue,int hValue,int iValue,int jValue)
+RenderItem(float sx, float sy, float w, float h, int Type, int Level, int Option1, int ExtOption,int Rotate)
+SendMessageClient(char* text)
+LockPlayerWalk()
+UnlockPlayerWalk()
+DrawTooltip(int x, int y, char* text)
+SetLockInterfaces()
+SetUnlockInterfaces()
+LuaGetLanguage() return string
+PlaySound(int id)
+StopSound(int id)
+GLSwitchBlend()
+GLSwitch()
+GetWideX() return integer 
+GetWindowWidthAdd() return integer 
+RenderText2(int aValue,int bValue,std::string cValue,int dValue,int eValue)
+
+
+-- ### PACKET FUNCTIONS ###
+CreatePacket(packetName, packet) -> create packet for send main
+SetDwordPacket(packetName, position) -> set value dword
+GetDwordPacket(packetName, position) -> get value dword
+SetWordPacket(packetName, position) -> set value word
+GetWordPacket(packetName, position) -> get value word
+SetBytePacket(packetName, position) -> set value byte
+GetBytePacket(packetName, position) -> get value byte
+SetCharPacket(packetName, string) -> set a string with a maximum of 10 characters
+GetCharPacket(packetName, position) -> get string
+SetCharPacketLength(packetName, string, length) -> set a string the size you set
+GetCharPacketLength(packetName, string, length) -> get a string
+SendPacket(packetName, aIndex) -> send packet for player
+ClearPacket(packetName) -> always when you create a packet you need clear!
