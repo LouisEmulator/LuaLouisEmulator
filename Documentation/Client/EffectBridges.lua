fdde2b4b18574c6cf92708e3fe2166f1d1f38151
Luis Fernando Más Ortiz <louisteam@hotmail.com>
2025-04-16 08:41:29 -0300

    effect doc

---
+++ Documentation/Client/EffectBridges.lua
@@ -0,0 +1,17 @@
+BridgeFunctionAttach('ItemEffect','TemplateScript_ItemEffect')
+
+
+function TemplateScript_ItemEffect(ItemIndex,BmdModel,VecPosX,VecPosY,VecPosZ,ObjItem,ObjModelIndex)
+	-- ### Argument information: ###
+	-- ItemIndex	 = Item indem (Group*512+ItemID)
+	-- BmdModel		 = Bdm struct index from item.BmdModel
+	-- VecPosX		 = Owner Object Position X
+	-- VecPosX		 = Owner Object Position Y
+	-- VecPosX		 = Owner Object Position Z
+	-- ObjItem		 = Item objetct struct
+	-- ObjModelIndex = Owner objetct struct
+
+	-- ### Bridge information: ###
+	-- Called in real-time effect item function.
+end
+
---
+++ Documentation/Client/EffectFunctions.lua
@@ -0,0 +1,7 @@
+-- ### ITEM EFFECT FUNCTIONS ###
+TransformPosition(BMD,Link,PosX,PosY,PosZ,Type) --Type 1 add World position coordenatie
+TransformByObjectBone(BMD,ObjectItem,Link)
+CreateParticle(ImageID,SubType,Scale,LightR,LightG,LightB,ObjectStruct) 
+CreateSprite(ImageID,Scale,LightR,LightG,LightB,ObjectStruct,Rotation,SubType) 
+CreateEffect(ImageID,SubType,LightX,LightY,LightZ,ObjectStruct,Size)
+MapTime() return integer
